e9b0f383b9d2cc1ae6287653097a002c42521e4d
nikolaspapirniywork <nikolaspapirniywork@gmail.com>
2013-09-25 11:39:34 +0300

    Coursera project edition changed and submitted

--- week01/src/main/scala/recfun/Main.scala
+++ week01/src/main/scala/recfun/Main.scala
@@ -1,19 +1,33 @@
-
-import scala.annotation.tailrec
+package recfun
+import common._
 
 object Main {
-  def pascal(col: Int, row: Int): Int = {
-    if (col <= 0 || row <= 0 || row <= col) 1
-    else pascal(col, row - 1) + pascal(col - 1, row - 1)
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
   }
 
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int =
+    if (c <= 0 || r <= 0 || r <= c) 1
+    else pascal(c, r - 1) + pascal(c - 1, r - 1)
+
+  /**
+   * Exercise 2
+   */
   def balance(chars: List[Char]): Boolean = {
     def loopOverChars(chars: List[Char], countOfOpenedParantheses: Int): Boolean = {
       if (chars.isEmpty)
         countOfOpenedParantheses == 0
       else {
-        var firstChar = chars.head
-        var n =
+        def firstChar = chars.head
+        def n =
           if (firstChar == '(') countOfOpenedParantheses + 1
           else if (firstChar == ')') countOfOpenedParantheses - 1
           else countOfOpenedParantheses
@@ -25,9 +39,12 @@ object Main {
     loopOverChars(chars, 0)
   }
 
+  /**
+   * Exercise 3
+   */
   def countChange(money: Int, coins: List[Int]): Int = {
     def countChangeAfterCheck(money: Int, coins: List[Int]): Int = {
-      var eligibleCoins = coins.filter(_ <= money)
+      def eligibleCoins = coins.filter(_ <= money)
 
       if (money == 0)
         1
