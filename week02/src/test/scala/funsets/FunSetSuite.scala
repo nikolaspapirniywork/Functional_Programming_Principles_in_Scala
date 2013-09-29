4fed5ee90d23c616504301a90752e6e235e2bbcf
Николай Пипирный <nikolaspapirniywork@gmail.com>
2013-09-29 20:04:04 +0300

    Week 2 assignment

---
+++ week02/src/test/scala/funsets/FunSetSuite.scala
@@ -0,0 +1,180 @@
+package funsets
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+/**
+ * This class is a test suite for the methods in object FunSets. To run
+ * the test suite, you can either:
+ * - run the "test" command in the SBT console
+ * - right-click the file in eclipse and chose "Run As" - "JUnit Test"
+ */
+@RunWith(classOf[JUnitRunner])
+class FunSetSuite extends FunSuite {
+
+
+  /**
+   * Link to the scaladoc - very clear and detailed tutorial of FunSuite
+   *
+   * http://doc.scalatest.org/1.9.1/index.html#org.scalatest.FunSuite
+   *
+   * Operators
+   * - test
+   * - ignore
+   * - pending
+   */
+
+  /**
+   * Tests are written using the "test" operator and the "assert" method.
+   */
+  test("string take") {
+    val message = "hello, world"
+    assert(message.take(5) == "hello")
+  }
+
+  /**
+   * For ScalaTest tests, there exists a special equality operator "===" that
+   * can be used inside "assert". If the assertion fails, the two values will
+   * be printed in the error message. Otherwise, when using "==", the test
+   * error message will only say "assertion failed", without showing the values.
+   *
+   * Try it out! Change the values so that the assertion fails, and look at the
+   * error message.
+   */
+  test("adding ints") {
+    assert(1 + 2 === 3)
+  }
+
+
+  import funsets.FunSets._
+
+  test("contains is implemented") {
+    assert(contains(x => true, 100))
+  }
+
+  test("contains all = true") {
+    assert(containsAll(x => x > 0, List(1, 2, 3, 4)))
+  }
+
+  test("contains all = false") {
+    assert(!containsAll(x => x > 0, List(1, 2, 3, -4)))
+  }
+
+  test("positive set contains element") {
+    assert(contains(x => (x > 0), 101))
+  }
+
+  test("positive set doesn't contain element") {
+    assert(contains(x => (x > 0), -1) == false)
+  }
+
+  /**
+   * When writing tests, one would often like to re-use certain values for multiple
+   * tests. For instance, we would like to create an Int-set and have multiple test
+   * about it.
+   *
+   * Instead of copy-pasting the code for creating the set into every test, we can
+   * store it in the test class using a val:
+   *
+   * val s1 = singletonSet(1)
+   *
+   * However, what happens if the method "singletonSet" has a bug and crashes? Then
+   * the test methods are not even executed, because creating an instance of the
+   * test class fails!
+   *
+   * Therefore, we put the shared values into a separate trait (traits are like
+   * abstract classes), and create an instance inside each test method.
+   *
+   */
+
+  trait TestSets {
+    val s1 = singletonSet(1)
+    val s2 = singletonSet(2)
+    val s3 = singletonSet(3)
+  }
+
+  /**
+   * This test is currently disabled (by using "ignore") because the method
+   * "singletonSet" is not yet implemented and the test would fail.
+   *
+   * Once you finish your implementation of "singletonSet", exchange the
+   * function "ignore" by "test".
+   */
+  test("singletonSet(1) contains 1") {
+
+    /**
+     * We create a new instance of the "TestSets" trait, this gives us access
+     * to the values "s1" to "s3". 
+     */
+    new TestSets {
+      /**
+       * The string argument of "assert" is a message that is printed in case
+       * the test fails. This helps identifying which assertion failed.
+       */
+      assert(contains(s1, 1), "Singleton")
+    }
+  }
+
+  test("union contains all elements") {
+    new TestSets {
+      val s = union(s1, s2)
+      assert(contains(s, 1), "Union 1")
+      assert(contains(s, 2), "Union 2")
+      assert(!contains(s, 3), "Union 3")
+    }
+  }
+
+  test("intersect contains all elements") {
+    val intersected = intersect(x => x > 0, y => y > 10)
+
+    assert(contains(intersected, 11))
+    assert(contains(intersected, 100))
+    assert(!contains(intersected, 10))
+    assert(!contains(intersected, 9))
+  }
+
+  test("diff of two sets - positive less than 100") {
+    val diffSet = diff(x => x > 0, y => y > 100)
+
+    assert(!contains(diffSet, -100))
+    assert(!contains(diffSet, 0))
+    assert(!contains(diffSet, 101))
+    assert(contains(diffSet, 10))
+    assert(!contains(diffSet, 500))
+  }
+
+  test("subset of a set - even positives") {
+    val subset = filter(x => x > 0, y => (y % 2 == 0))
+
+    assert(contains(subset, 2))
+    assert(!contains(subset, 3))
+  }
+
+  test("subset of a set - bigger than 0 and less than 100") {
+    val subset = filter(x => x > 0, y => y < 100)
+
+    assert(!contains(subset, 0))
+    assert(!contains(subset, 100))
+    assert(contains(subset, 50))
+  }
+
+  test("For all integers (0,10) satisfy predicate that integers greater than 0")(assert(forall(x => (x > 0 && x < 10), x => x > 0)))
+
+  test("For all even integers does not satisfy predicate that integers are negative")(assert(!forall(x => (x % 2 == 0), x => x < 0)))
+
+  test("For positive integers does exist even integer")(assert(exists(x => x > 0, x => (x % 2 == 0))))
+
+  test("For negative integers does exist positive integer")(assert(!exists(x => x < 0, x => (x > 0))))
+
+  test("Map transforms set by applying function f") {
+    val mappedValues: FunSets.Set = map(union(x => x > 0, y => y < 10), x => x * 2)
+    // 0 2 4 6 8 10 12 14 16 18
+    //assert(containsAll(mappedValues, List(0, 2, 4, 6, 8, 10, 12, 14, 16, 18)))
+    assert(contains(mappedValues, 0))
+    assert(contains(mappedValues, 2))
+    assert(!contains(mappedValues, -1))
+    assert(!contains(mappedValues, 11))
+  }
+}
